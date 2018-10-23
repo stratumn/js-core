90a8420c6ee819d6f6f59f54658ea5e444251adc
t-bast <bastien@stratumn.com>
2018-10-23 11:45:20 +0200

    fossilizer: get info

--- packages/fossilizer-client/src/fossilizerClient.spec.ts
+++ packages/fossilizer-client/src/fossilizerClient.spec.ts
@@ -14,10 +14,66 @@
   limitations under the License.
 */
 
-import { FossilizerClient } from './fossilizerClient';
+import axios from 'axios';
+import { FossilizerHttpClient } from './fossilizerClient';
 
-describe('fossilizer client', () => {
-  it('says hello', () => {
-    expect(new FossilizerClient().greet()).toBe('hello');
+jest.mock('axios');
+
+describe('fossilizer http client', () => {
+  let axiosMock: jest.SpyInstance;
+
+  afterEach(() => {
+    axiosMock.mockRestore();
+  });
+
+  describe('info', () => {
+    it('throws in case of network error', async () => {
+      const client = new FossilizerHttpClient('http://127.0.0.1:6001');
+
+      axiosMock = jest.spyOn(axios, 'get');
+      axiosMock.mockRejectedValue('network failure');
+
+      try {
+        await client.info();
+        expect(true).toBeFalsy();
+      } catch (err) {
+        expect(axiosMock).toHaveBeenCalled();
+        expect(err).toBe('network failure');
+      }
+    });
+
+    it('throws if status code is not ok', async () => {
+      const client = new FossilizerHttpClient('http://localhost:6000');
+
+      axiosMock = jest.spyOn(axios, 'get');
+      axiosMock.mockResolvedValue({ status: 404, statusText: 'Not Found' });
+
+      try {
+        await client.info();
+        expect(true).toBeFalsy();
+      } catch (err) {
+        expect(axiosMock).toHaveBeenCalled();
+        expect(err).toEqual(new Error('HTTP 404: Not Found'));
+      }
+    });
+
+    it('returns fossilizer info', async () => {
+      const client = new FossilizerHttpClient('https://fossilize.stratumn.com');
+      const fossInfo = {
+        description: 'Fossilizes data on the Bitcoin blockchain',
+        name: 'BtcFossilizer'
+      };
+
+      axiosMock = jest.spyOn(axios, 'get');
+      axiosMock.mockResolvedValue({
+        data: {
+          adapter: fossInfo
+        },
+        status: 200
+      });
+
+      const res = await client.info();
+      expect(res).toBe(fossInfo);
+    });
   });
 });

4a8b86d6e4a47241f748676da28999ede9b9c41b
t-bast <bastien@stratumn.com>
2018-10-26 16:49:00 +0200

    store: create client interface

---
+++ packages/store-client/src/client.ts
@@ -0,0 +1,43 @@
+/*
+  Copyright 2018 Stratumn SAS. All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import { Link, Segment } from '@stratumn/js-chainscript';
+
+/**
+ * IStoreClient provides access to the Stratumn Chainscript Store API.
+ * It lets your application store and retrieve Chainscript segments.
+ */
+export interface IStoreClient {
+  /**
+   * Returns unstructured information about the store like its name,
+   * description, commit hash, etc.
+   */
+  info(): Promise<any>;
+
+  /**
+   * Create a new link in the Chainscript store.
+   * @param link new link that should be stored.
+   * @returns the segment encapsulating this link.
+   */
+  createLink(link: Link): Promise<Segment>;
+
+  /**
+   * Get a segment from its link hash.
+   * @param linkHash hex-encoded link hash.
+   * @returns the segment with its evidences (if any).
+   */
+  getSegment(linkHash: string): Promise<Segment>;
+}
--- packages/store-client/src/index.ts
+++ packages/store-client/src/index.ts
@@ -13,3 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
+
+import { IStoreClient } from './client';
+
+export { IStoreClient };

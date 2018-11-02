492edc0ea24186b436f69e61e5641979664c5ae7
t-bast <bastuc@hotmail.fr>
2018-11-02 14:52:27 +0100

    Replace App by a MapExplorer component.

--- packages/react-map-explorer/src/MapExplorer.test.tsx
+++ packages/react-map-explorer/src/MapExplorer.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { MapExplorer } from './MapExplorer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MapExplorer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ packages/react-map-explorer/src/MapExplorer.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+/**
+ * A component to load and display Chainscript process maps.
+ */
+export class MapExplorer extends Component {
+  public render() {
+    return <h1>WIP</h1>;
+  }
+}
